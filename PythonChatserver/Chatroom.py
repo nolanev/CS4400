65f3bda5327d129fda0a61ef9514d3370dc66adf
Eva Nolan <nolanev@tcd.ie>
2017-11-17 19:00:42 +0000

    no message control

---
+++ PythonChatserver/Chatroom.py
@@ -0,0 +1,30 @@
+#Class of type chatroom
+#with functions addclient, removeclient
+
+
+class Chatroom:
+    def __init__(self, roomName, roomID):
+        self.roomID = roomID
+        self.roomName = roomName
+        self.clients = {}
+        self.mutex = Lock()
+	
+    def __str__(self):
+        return self.join_id
+		
+	def addClient(self, clientName, joinID):
+		with self.mutex:
+			if clientName not in self.clients
+			self.clients{joinID}=clientName
+			#added message
+			else:
+			#error
+	
+	
+	def removeClient(self, clientName, joinID):
+		with self.mutex:
+			if clientName in self.clients
+				self.clients{joinID}=NULL
+				#removed message
+			else:
+				#error
---
+++ PythonChatserver/Parse.py
@@ -0,0 +1,11 @@
+#parse function
+#join parse
+#add client parse
+#remove client parse
+
+	#send message to chatroom
+	#clientSocket.send("JOIN_CHATROOM: ", chatroomname)
+	#clientSocket.send(" CLIENT_IP: ", host)
+	#clientSocket.send(" PORT: ",port)
+	#clientSocket.send(" CLIENT_NAME: ", myname)
+	
